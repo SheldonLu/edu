0634f085ed5b43d1f1cd0a96f0b1703ef29e0ffd
lubing <w.t.start@gmail.com>
2013-04-08 08:42:32 +0800

    login view finish

--- edu/eduViewController.m
+++ edu/eduViewController.m
@@ -23,10 +23,18 @@
 	// Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"jz_02.png"]];
    // loginView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"jz_10.png"]];
-    [self.loginNameView setText:@"wll"];
-    [self.loginPwdView setText:@"sRYVHy>m6Tgx"];
-    isRemember = false;
-    isLogin = false;
+   
+    isRemember = [[NSUserDefaults standardUserDefaults] boolForKey:@"isRemember"];
+    isLogin = [[NSUserDefaults standardUserDefaults] boolForKey:@"isLogin"];
+//     [self.loginPwdView setText:@"sRYVHy>m6Tgx"];
+    if (isRemember) {
+        
+//        [self.loginNameView setText:@"wll"];
+//        [self.loginPwdView setText:@"sRYVHy>m6Tgx"];
+        [self.rememberPwd setBackgroundImage:[UIImage imageNamed:@"btn_check_on_disabled_holo_light.png"] forState:UIControlStateNormal];
+        [self.loginNameView setText:[[NSUserDefaults standardUserDefaults] stringForKey:@"loginName"]];
+        [self.loginPwdView setText:[[NSUserDefaults standardUserDefaults] stringForKey:@"loginPwd"]];
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,7 +42,22 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:YES];
+    if (isLogin) {
+         [self performSelector:@selector(autoLoginIn) withObject:nil afterDelay:0.2];
+    }
+   
 
+}
+-(void)autoLoginIn
+{
+    NSLog(@"loginNameLength%c",self.loginNameView.text.length);
+    if (self.loginPwdView.text.length && self.loginNameView.text.length) {
+        [self click:nil];
+    }
+}
 -(void)loginWithName:(NSString *)name password:(NSString *)password
 {
     [SVProgressHUD showWithMaskType:SVProgressHUDMaskTypeBlack];
@@ -54,6 +77,18 @@
         int errorCode = [[JSON objectForKey:@"ErrorCode"] intValue];
         if (errorCode == 0) {
             [SVProgressHUD dismissWithSuccess:@"登入成功" afterDelay:1];
+            id photoLogin = [JSON objectForKey:@"photologin"];
+            if (isRemember) {
+                [[NSUserDefaults standardUserDefaults] setValue:name forKey:@"loginName"];
+                [[NSUserDefaults standardUserDefaults] setValue:password forKey:@"loginPwd"];
+            }else{
+                [[NSUserDefaults standardUserDefaults] setValue:nil forKey:@"loginName"];
+                [[NSUserDefaults standardUserDefaults] setValue:nil forKey:@"loginPwd"];
+            }
+            [[NSUserDefaults standardUserDefaults] setObject:photoLogin forKey:@"photoLogin"];
+            [[NSUserDefaults standardUserDefaults] setBool:isRemember forKey:@"isRemember"];
+            [[NSUserDefaults standardUserDefaults] setBool:isLogin forKey:@"isLogin"];
+            [[NSUserDefaults standardUserDefaults] synchronize];
             UIStoryboard *mainStoryBoard=[UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil];
             MainViewController *mv=[mainStoryBoard instantiateViewControllerWithIdentifier:@"MainViewController"];
             mv.modalTransitionStyle=UIModalTransitionStyleFlipHorizontal;
